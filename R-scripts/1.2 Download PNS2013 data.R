c0ab27f9208a879ab7683308aef5ada84c743c7b
Rafael Pereira <rafa.pereira.br@gmail.com>
2016-08-05 16:36:25 +0100

    renamed file

--- R-scripts/1.2 Download PNS2013 data.R
+++ R-scripts/1.2 Download PNS2013 data.R
@@ -455,8 +455,8 @@ table(pns2013$edugroup)
                             weights=~V0029, #PesoPessoa: usar peso original
                             nest = TRUE)
 
-## Agora È preciso pÛs-estratificar:
-## A definiÁ„o dos pÛs-estratos e os totais populacionais usados s„o dados por:
+## Agora √© preciso p√≥s-estratificar:
+## A defini√ß√£o dos p√≥s-estratos e os totais populacionais usados s√£o dados por:
 
 ## post-estratification of sample design
   post_pop <- unique( subset(pns2013Det, select= c(V00293,V00292) ))
@@ -483,7 +483,7 @@ table(pns2013$edugroup)
 #Total population of Brazil above 18 years old (146.3 million)
 svytotal (~vcount , sample.pns13.pos)
 
-# Check against Gr·fico 1, % of ind. above 18yearsold whith good+very good health (self-assessment)
+# Check against Gr√°fico 1, % of ind. above 18yearsold whith good+very good health (self-assessment)
       #ok - Brasil 66.1%
       svymean(~factor(N001<3), design= sample.pns13.pos) # 66.1%
       
@@ -505,14 +505,14 @@ svytotal (~vcount , sample.pns13.pos)
     svyby(~physicallyactive30, ~region+C006, design= sample.pns13.pos,  vartype="ci",  level = 0.95,  svyciprop)
     
     
-    #Race - Gr·fico 12 
+    #Race - Gr√°fico 12 
     svyby(~physicallyactive30, ~C009, design= sample.pns13.pos,  vartype="ci",  level = 0.95,  svyciprop)
     
  
     
-# Tabela 18.2 - Rendimento mensal mÈdio habitual de todos os trabalhos de 18 anos ou mais de
-# idade que possui regime de trabalho n„o noturno, por sexo, com indicaÁ„o do intervalo de
-# confianÁa de 95%, segundo as Grandes Regiıes - 2013
+# Tabela 18.2 - Rendimento mensal m√©dio habitual de todos os trabalhos de 18 anos ou mais de
+# idade que possui regime de trabalho n√£o noturno, por sexo, com indica√ß√£o do intervalo de
+# confian√ßa de 95%, segundo as Grandes Regi√µes - 2013
     
     svyby(~E01602, ~region+v0302, 
           design= subset(sample.pns13.pos, M005==2),  # no night shift
